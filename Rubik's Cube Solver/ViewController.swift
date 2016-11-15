3b0faabda819112f28d8c60684b59a43ad7b5b30
ChristopherStanley14 <christopherstanley@christopherstanleymbp.local>
2016-11-15 13:37:15 -0500

    Initial Commit

---
+++ Rubik's Cube Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rubik's Cube Solver
+//
+//  Created by Christopher Stanley on 11/15/16.
+//  Copyright © 2016 Christopher Stanley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
